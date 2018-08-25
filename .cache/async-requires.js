0f5517bead06c743e953f6565b3161345ab41cd5
Michael Semyonov <naservere@gmail.com>
2018-08-25 18:59:21 +0300

    Update Gatsby

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,16 +2,17 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": require("gatsby-module-loader?name=component---node-modules-gatsby-plugin-offline-app-shell-js!/Users/naservere/code/serviceorderhub-docs-v2/node_modules/gatsby-plugin-offline/app-shell.js"),
   "component---src-templates-lesson-jsx": require("gatsby-module-loader?name=component---src-templates-lesson-jsx!/Users/naservere/code/serviceorderhub-docs-v2/src/templates/lesson.jsx"),
   "component---src-templates-post-jsx": require("gatsby-module-loader?name=component---src-templates-post-jsx!/Users/naservere/code/serviceorderhub-docs-v2/src/templates/post.jsx"),
   "component---src-templates-category-jsx": require("gatsby-module-loader?name=component---src-templates-category-jsx!/Users/naservere/code/serviceorderhub-docs-v2/src/templates/category.jsx"),
-  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/naservere/code/serviceorderhub-docs-v2/.cache/dev-404-page.js"),
   "component---src-pages-about-jsx": require("gatsby-module-loader?name=component---src-pages-about-jsx!/Users/naservere/code/serviceorderhub-docs-v2/src/pages/about.jsx"),
   "component---src-pages-index-jsx": require("gatsby-module-loader?name=component---src-pages-index-jsx!/Users/naservere/code/serviceorderhub-docs-v2/src/pages/index.jsx")
 }
 
 exports.json = {
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/layout-index.json"),
+  "offline-plugin-app-shell-fallback.json": require("gatsby-module-loader?name=path---offline-plugin-app-shell-fallback!/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/offline-plugin-app-shell-fallback.json"),
   "lesson-five.json": require("gatsby-module-loader?name=path---lesson-five!/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/lesson-five.json"),
   "post-one.json": require("gatsby-module-loader?name=path---post-one!/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/post-one.json"),
   "lesson-four.json": require("gatsby-module-loader?name=path---lesson-four!/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/lesson-four.json"),
@@ -20,7 +21,6 @@ exports.json = {
   "lesson-three.json": require("gatsby-module-loader?name=path---lesson-three!/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/lesson-three.json"),
   "lesson-two.json": require("gatsby-module-loader?name=path---lesson-two!/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/lesson-two.json"),
   "categories-tech.json": require("gatsby-module-loader?name=path---categories-tech!/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/categories-tech.json"),
-  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/dev-404-page.json"),
   "about.json": require("gatsby-module-loader?name=path---about!/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/about.json"),
   "index.json": require("gatsby-module-loader?name=path---index!/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/index.json")
 }
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,16 +7,17 @@ exports.layouts = {
 }
 
 exports.components = {
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Users/naservere/code/serviceorderhub-docs-v2/node_modules/gatsby-plugin-offline/app-shell.js")),
   "component---src-templates-lesson-jsx": preferDefault(require("/Users/naservere/code/serviceorderhub-docs-v2/src/templates/lesson.jsx")),
   "component---src-templates-post-jsx": preferDefault(require("/Users/naservere/code/serviceorderhub-docs-v2/src/templates/post.jsx")),
   "component---src-templates-category-jsx": preferDefault(require("/Users/naservere/code/serviceorderhub-docs-v2/src/templates/category.jsx")),
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/naservere/code/serviceorderhub-docs-v2/.cache/dev-404-page.js")),
   "component---src-pages-about-jsx": preferDefault(require("/Users/naservere/code/serviceorderhub-docs-v2/src/pages/about.jsx")),
   "component---src-pages-index-jsx": preferDefault(require("/Users/naservere/code/serviceorderhub-docs-v2/src/pages/index.jsx"))
 }
 
 exports.json = {
   "layout-index.json": require("/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/layout-index.json"),
+  "offline-plugin-app-shell-fallback.json": require("/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/offline-plugin-app-shell-fallback.json"),
   "lesson-five.json": require("/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/lesson-five.json"),
   "post-one.json": require("/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/post-one.json"),
   "lesson-four.json": require("/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/lesson-four.json"),
@@ -25,7 +26,6 @@ exports.json = {
   "lesson-three.json": require("/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/lesson-three.json"),
   "lesson-two.json": require("/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/lesson-two.json"),
   "categories-tech.json": require("/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/categories-tech.json"),
-  "dev-404-page.json": require("/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/dev-404-page.json"),
   "about.json": require("/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/about.json"),
   "index.json": require("/Users/naservere/code/serviceorderhub-docs-v2/.cache/json/index.json")
 }
